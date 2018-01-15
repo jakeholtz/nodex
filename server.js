436b150460b144969e84a1ba681786cdfc1e02c3
Jake Holtz <jahholtz@gmail.com>
2018-01-14 20:25:49 -0800

    Connecting to API

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-const app = require('express')();
-const client = require('path').join(__dirname + '/client.html');
+var express = require('express');
+var app = express();
+var request = require('request');
+var router = express.Router();
+app.listen(3000, () => console.log("Listening in on port 3000"));
 
+var client = require('path').join(__dirname + '/client.html');
+var key = require('./key.js');
+
+var geoAPIpath = 'https://www.googleapis.com/geolocation/v1/geolocate?key=' + key;
 app.get('/', (req, res) => res.sendFile(client));
 
-app.listen(3000, () => console.log("Listening in on port 3000"));
+request.post(
+  { url: geoAPIpath },
+  function(err, res, body){
+    console.log(err ? "Error connecting to Google's API" : body);
+  }
+)
